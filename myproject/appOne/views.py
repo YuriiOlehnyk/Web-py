b948e3519508bb341ad8100a9e34b6033402db62
Yurii Olekhnyk <yurii.olehnyk@gmail.com>
2026-05-14 17:52:43 +0200

    Adding docker file

--- myproject/appOne/views.py
+++ myproject/appOne/views.py
@@ -1,10 +1,15 @@
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'appOne/index.html')
+    ip = request.META.get('REMOTE_ADDR')
+    return render(request, 'appOne/index.html', {'ip': ip})
 
 def connection_check(request):
     return HttpResponse('<h1>Hello, connection established successfully!</h1>')
